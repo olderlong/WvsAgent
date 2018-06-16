ddbb63a2f6846ffa004dd83d0bc63be771ce1dca
lyz <olderlong@163.com>
2018-06-16 17:08:46 +0800

    代码优化

--- run.py
+++ run.py
@@ -1,30 +1,30 @@
 #! /usr/bin/env python
 # _*_ coding:utf-8 _*_
+import logging
 
 from app import CCAgent, AppScanControl
 from app.lib import common_msg, msg_bus
 
+logger = logging.getLogger("Agent")
 agent = CCAgent(name="AppScan", port=5555, cc_server_address=("192.168.3.2", 6666))
+
 appscan = AppScanControl()
 
 
 def agent_run():
-    agent = CCAgent(name="AppScan", port=4444, cc_server_address=("192.168.3.2", 6666))
-    appscan = AppScanControl()
-    print("Agent running...")
+    logger.info("Agent running...")
     agent.start()
     agent.join()
 
 
-def agent_stop(msg):
-    # print(event.dict)
+def agent_stop():
     print("agent exit...")
     appscan.stop_scan()
     agent.stop()
     msg_bus.stop()
 
+
 msg_bus.add_msg_listener(common_msg.MSG_AGENT_EXIT, agent_stop)
 
 if __name__ == '__main__':
-
     agent_run()
