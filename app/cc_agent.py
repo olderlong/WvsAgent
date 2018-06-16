94b56c6a03527d69948d6421a65410c1326d29ff
lyz <olderlong@163.com>
2018-06-16 19:09:15 +0800

    代码优化，独立心跳包发送函数

--- app/cc_agent.py
+++ app/cc_agent.py
@@ -6,6 +6,7 @@ import json
 import logging
 from app.lib import UDPEndPoint, msg_bus, common_msg, STATE_UPDATE_INTERVAL
 
+
 logger = logging.getLogger("Agent")
 
 
@@ -17,9 +18,8 @@ class CCAgent(UDPEndPoint):
         self.__heartbeat_thread = threading.Thread(target=self.__send_heartbeat_pkg)
         self.__is_heartbeat_thread_running = threading.Event()  # 用于停止线程的标识
 
-        # self.EVENT_SCAN_RESULT_SEND = "SendResultSend"
-        # self.event_scan_result_send = Event(type_=self.EVENT_SCAN_RESULT_SEND)
         msg_bus.add_msg_listener(common_msg.MSG_SCAN_RESULT_SEND, self.send_result)
+        msg_bus.add_msg_listener(common_msg.MSG_WVS_STATE, self.send_state)
 
         super(CCAgent, self).__init__(port=port, handler=self.recv_data_handler)
 
@@ -28,12 +28,11 @@ class CCAgent(UDPEndPoint):
             self.cc_server = address
 
         pkg_obj = dict(json.loads(str(data, encoding='utf-8')))
-
         #针对Data中字段进行分析处理
         try:
             if pkg_obj["Type"] == "WVSCommand":  #命令数据包
                 data = pkg_obj["Data"]
-                logger.info(pkg_obj["Data"])
+                # logger.info(pkg_obj["Data"])
                 common_msg.msg_wvs_command.data = data
                 msg_bus.send_msg(common_msg.msg_wvs_command)
 
@@ -48,8 +47,18 @@ class CCAgent(UDPEndPoint):
         except KeyError as e:
             logger.error("收到来自{}的未知类型数据——{}".format(address, data))
 
-    def send_state(self, state_json):
+    def send_state(self, msg):
+        """
+        还存在bug
+        :param msg:
+        :return:
+        """
+        state_json = msg.data
+        logger.info(state_json)
         self.send_json_to(state_json,  self.cc_server)
+
+    def send_heartbeat(self, data):
+        self.send_json_to(data,  self.cc_server)
 
     def send_result(self, msg):
         result_json = msg.data
@@ -81,11 +90,27 @@ class CCAgent(UDPEndPoint):
                     "State":  "Online"
                 }
             }
-            self.send_state(state)
+            self.send_heartbeat(state)
             time.sleep(self.__heartbeat_tm)
 
 
 if __name__ == '__main__':
     agent = CCAgent(cc_server_address=("192.168.3.2", 6666))
+    state = {
+        "Type": "WVSState",
+        "Data": {
+            "Name": "Appscan",
+            "Address": agent.address,
+            "Timestamp": time.time(),
+            "State": u"初始化"
+        }
+    }
     agent.start()
+
+    from app.wvs_state import WvsState
+    wvsstate = WvsState(wvs_name="Appscan", address=("192.168.3.10", 5555))
+    wvsstate.update_state()
+    # common_msg.msg_wvs_state.data = state
+    # msg_bus.send_msg(common_msg.msg_wvs_state)
     agent.join()
+
