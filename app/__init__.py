0896c30b60d01c089c838b318ef6013bc2421436
lyz <olderlong@163.com>
2018-06-16 19:11:09 +0800

    更改日志文件名格式

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,12 @@ import time
 import sys
 import os
 
+from app.lib import *
+
 from .cc_agent import CCAgent
 from .wvs_control_base import WVSControlBase
 from .app_scan_control import AppScanControl
+from .wvs_state import WvsState
 
 
 def __init_logger(name):
@@ -17,7 +20,11 @@ def __init_logger(name):
     formatter = logging.Formatter('%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s >>> %(message)s')
     # 文件日志
     app_path = os.getcwd()
-    file_handler = logging.FileHandler(os.path.join(app_path, "log/{}.log".format(time.time())))
+    file_handler = logging.FileHandler(
+        os.path.join(
+            app_path,
+            "readme.md/{}.readme.md".format(
+                time.strftime("%Y_%m_%d_%H_%M_%S", time.gmtime(time.time())))))
     file_handler.setFormatter(formatter)  # 可以通过setFormatter指定输出格式
 
     # 控制台日志
@@ -32,5 +39,4 @@ def __init_logger(name):
 __init_logger("Agent")
 
 
-
-__all__ = ["CCAgent", "WVSControlBase", "AppScanControl"]
+__all__ = ["CCAgent", "WVSControlBase", "AppScanControl", "WvsState"]
